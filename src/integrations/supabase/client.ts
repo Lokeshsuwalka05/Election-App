d649af3e3eb436ce04ae0858d111b37f26307c40
unknown <lokeshsuwalka2002@gmail.com>
2025-05-30 16:25:31 +0530

    JWT issue Resolved

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
-import type { Database } from './types';
 
-const SUPABASE_URL = "https://tswieklnhrlbvhuqbfev.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzd2lla2xuaHJsYnZodXFiZmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2OTk5NjksImV4cCI6MjA2MjI3NTk2OX0.biRiZe2Z6Bwfwn8Suval6SUmg_f3tJKPaMvaqWFuy-Y";
+if (!import.meta.env.VITE_SUPABASE_URL) {
+  throw new Error('Missing env.VITE_SUPABASE_URL');
+}
+if (!import.meta.env.VITE_SUPABASE_ANON_KEY) {
+  throw new Error('Missing env.VITE_SUPABASE_ANON_KEY');
+}
 
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
-
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
